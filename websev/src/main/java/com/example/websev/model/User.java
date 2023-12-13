d3442228089c4cd880f3aac1c8a957f0a4e6851e
Charb60 <78400746+Charb60@users.noreply.github.com>
2023-12-13 11:29:49 +0700

    firstcommit

---
+++ websev/src/main/java/com/example/websev/model/User.java
@@ -0,0 +1,50 @@
+package com.example.websev.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private Long age;
+    private byte[] bytes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setImage(byte[] bytes) {
+        this.bytes = bytes;
+    }
+}
